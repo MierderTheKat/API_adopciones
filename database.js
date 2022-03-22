4af8a723e93877537e20111b966447e6df886bf9
MierderTheKat <lamierdasjj2@hotmail.com>
2022-03-22 15:12:18 -0600

    new commit

--- database.js
+++ database.js
@@ -1,9 +1,15 @@
 let config = {
-    host: "localhost",
-    user: "root",
-    password: "",
+    host: "us-cdbr-east-05.cleardb.net",
+    user: "bf71f0465966d4",
+    password: "7b529a90",
     port: 3306,
-    database: "adoption_system",
+    database: "heroku_b85782ff82565bf",
     secret_key: "k60j9d9xZywqpdjea"
 };
 exports.config = config;
+
+/*
+mysql://bf71f0465966d4:7b529a90@us-cdbr-east-05.cleardb.net/heroku_b85782ff82565bf?reconnect=true
+       |     user     |password|            host           |     database name    |
+
+*/
